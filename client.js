5b19c4e76a2caf4a0a7aee523962d0b8b316eb15
Kyle Fritz <kyle.p.fritz@gmail.com>
2011-09-24 08:50:49 -0400

    betabot works

--- client.js
+++ client.js
@@ -1,9 +1,27 @@
-var io = require('socket.io-client')
+//TODO: client's dependencies not reflected in package.json (because heroku hates them)
+var io = require('socket.io-client');
+var SerialPort = require('serialport').SerialPort;
 
-//var socket = io.connect('http://localhost:5000');
-var socket = io.connect('http://betabot.herokuapp.com');
-socket.on('angles:changed', function (data) {
-  console.log('angles are ',data);
+require('child_process').exec('ls /dev/tty.usbserial*',function(err,stdout,stderr){
+
+  //look up serial port dynamically
+  var port=stdout.trim();
+  console.log('connecting to',port);
+  var serialport=new SerialPort(port);
+
+  //var socket = io.connect('http://localhost:5000');
+  var socket = io.connect('http://betabot.herokuapp.com');
+  socket.on('angles:changed', function (data) {
+    //console.log('send changed angles');
+    serialport.write('f'+data['front']);
+    serialport.write('b'+data['back']);
+    serialport.write('t'+data['turn']);
+  });
+  socket.on('dance', function (data) {
+    console.log('send dance!!');
+    serialport.write('d');
+  });
 });
+
 console.log('client is listening!')
 
